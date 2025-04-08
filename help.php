dad82feda6e0a1245904ec9dcd0f7e04317fa36b
Thomas Skerbis <thomas.skerbis@mac.com>
2025-04-08 23:18:20 +0200

    Delete help.php

--- help.php
+++
@@ -1,6 +0,0 @@
-<h1>CSS Above The Fold</h1>
-<p>Das Addon kann für die Google-Insight-CSS-Optimierung verwendet werden (mehr zum Thema: <a target="_blank" href="https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery">https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery</a>)</p>
-<p>Das Addon merkt sich alle CSS-Regeln, die für das schnelle Rendern des sichtbaren Teils benötigt werden.
-Diese Regeln werden dann inline im &lt;head&gt; ausgegeben. Die restlichen Stylesheets können nach dem schliessenden &lt;html&gt;-Tag eingebunden werden.</p>
-<p>Das Addon merkt sich die Regeln pro Artikel (article_id), Sprache (clang_id) und Gerät (mobile, desktop).</p>
-<p>Repository: https://github.com/FriendsOfREDAXO/css_above_the_fold</p>
