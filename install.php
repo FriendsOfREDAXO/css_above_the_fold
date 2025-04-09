489c92e2ef2d9d9aec32cc26f161d883f299f3ca
Thomas Skerbis <thomas.skerbis@mac.com>
2025-04-09 09:44:47 +0200

    Update install.php

--- install.php
+++ install.php
@@ -8,13 +8,24 @@
 $cacheDir = rex_path::addonCache('css_above_the_fold');
 rex_dir::create($cacheDir);
 
-// JavaScript-Verzeichnis erstellen, falls noch nicht vorhanden
-$jsDir = rex_path::addon('css_above_the_fold', 'assets/js');
+// Assets-Verzeichnisstruktur erstellen
+$assetsDir = rex_path::addon('css_above_the_fold', 'assets');
+$jsDir = $assetsDir . '/js';
 rex_dir::create($jsDir);
 
-// Critical-Extractor JavaScript-Datei erstellen
+// Critical-Extractor JavaScript-Datei in das assets/js-Verzeichnis kopieren
 $jsFile = rex_path::addon('css_above_the_fold', 'assets/js/critical-extractor.js');
-$jsContent = <<<'EOD'
+$jsContent = file_get_contents(__DIR__ . '/critical-extractor.js.template');
+
+if ($jsContent) {
+    rex_file::put($jsFile, $jsContent);
+    echo rex_view::success('JavaScript-Datei erfolgreich erstellt: ' . $jsFile);
+} else {
+    echo rex_view::warning('Template-Datei für JavaScript nicht gefunden. Das AddOn könnte nicht vollständig funktionieren.');
+}
+
+// Erfolgsmeldung
+return true;
 /**
  * CSS Above The Fold - Critical CSS Extractor
  * 
